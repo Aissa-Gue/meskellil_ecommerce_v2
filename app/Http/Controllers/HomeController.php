9211708200bc1b1d0f6d04b7fe12f4da0c863b06
Aissa-Gue <aissastardz@gmail.com>
2025-08-30 07:16:10 +0100

    feat: add views functions

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -51,4 +51,9 @@ class HomeController extends Controller
         return view('home', compact(['newestProducts', 'bestSellingProducts', 'discountedProducts']));
     }
 
+    public function contact()
+    {
+        return view('contact');
+    }
+
 }
